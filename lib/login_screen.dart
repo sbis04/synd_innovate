5ce04ae4741f969ac474c66a6c74ded0d063419d
Souvik Biswas <sbis1999@gmail.com>
2019-08-29 09:49:19 +0530

    Creating login screen * Added some logos

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -3,8 +3,76 @@ import 'package:flutter/material.dart';
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Color(0xffFFCD00),
+    return Stack(
+      fit: StackFit.expand,
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+            color: Color(0xffFFCD00),
+          ),
+        ),
+        SafeArea(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 15,
+                  right: 15,
+                  left: 15,
+                ),
+                child: Image.asset('assets/images/logo.png'),
+              ),
+              Card(
+                elevation: 8,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                color: Colors.white70,
+                child: Column(
+                  children: <Widget>[
+                    Text('Login'),
+                    SizedBox(
+                      width: 300,
+                      child: FlatButton(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        color: Colors.white,
+                        child: Padding(
+                          padding: const EdgeInsets.all(6.0),
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: <Widget>[
+                                Image(
+                                    image: AssetImage(
+                                        "assets/images/google_logo.png"),
+                                    height: 35.0),
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 10),
+                                  child: Text(
+                                    'Sign in with Google',
+                                    style: TextStyle(
+                                      color: Colors.grey,
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
