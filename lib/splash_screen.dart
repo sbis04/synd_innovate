e646bceb0d74cc0c44da10ee3a3dc47ba748af1f
Souvik Biswas <sbis1999@gmail.com>
2019-08-29 08:13:34 +0530

    Initial commit

---
+++ lib/splash_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'login_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+      Duration(seconds: 3),
+      () => Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) {
+            return LoginScreen();
+          },
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              color: Color(0xffFFCD00),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              right: 15,
+              left: 15,
+            ),
+            child: Image.asset('assets/images/logo.png'),
+          ),
+        ],
+      ),
+    );
+  }
+}
