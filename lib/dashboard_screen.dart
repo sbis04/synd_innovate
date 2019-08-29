106328fd3ad4fda148d58df3004feb7ac7f67c58
Souvik Biswas <sbis1999@gmail.com>
2019-08-29 12:55:55 +0530

    Added GridView in dashboard

--- lib/dashboard_screen.dart
+++ lib/dashboard_screen.dart
@@ -4,44 +4,81 @@ import 'package:carousel_slider/carousel_slider.dart';
 class DashboardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      fit: StackFit.expand,
-      children: <Widget>[
-        Container(
-          decoration: BoxDecoration(
-            color: Color(0xffFFCD00),
+    return Scaffold(
+      bottomNavigationBar: BottomAppBar(),
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              color: Color(0xCCEFC622),
+            ),
           ),
-        ),
-        Column(
-          children: <Widget>[
-            Container(
-              child: Align(
-                alignment: Alignment.topCenter,
-                child: Card(
-                  margin: EdgeInsets.all(0),
-                  shape: RoundedRectangleBorder(
+          Column(
+            children: <Widget>[
+              Container(
+                child: Align(
+                  alignment: Alignment.topCenter,
+                  child: Card(
+                    margin: EdgeInsets.all(0),
+                    shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(30),
-                          bottomRight: Radius.circular(30))),
-                  elevation: 10,
-                  color: Color(0xffFFCD00),
-                  child: SafeArea(
-                    child: CarouselSlider(
-                      viewportFraction: 1.0,
-                      height: 200,
-                      items: <Widget>[
-                        Image.asset('assets/images/synd_disc.png'),
-                        Image.asset('assets/images/synd_disc.png'),
-                        Image.asset('assets/images/synd_disc.png'),
-                      ],
+                        bottomLeft: Radius.circular(30),
+                        bottomRight: Radius.circular(30),
+                      ),
+                    ),
+                    elevation: 10,
+                    color: Color(0xffFFCD00),
+                    child: SafeArea(
+                      child: CarouselSlider(
+                        viewportFraction: 1.0,
+                        height: 250,
+                        items: <Widget>[
+                          Image.asset('assets/images/synd_disc.png'),
+                          Image.asset('assets/images/synd_disc.png'),
+                          Image.asset('assets/images/synd_disc.png'),
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
-        ),
-      ],
+              Expanded(
+                child: Container(
+                  child: GridView.builder(
+                    gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2),
+                    itemCount: 10,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Card(
+                          color: Color(0xCC327E81),
+                          elevation: 5,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          child: Center(
+                            child: Text(
+                              'Savings Account',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
